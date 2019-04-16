66e0bf0bfa7485eb2dd20ea5761f03471a8f0210
MilonLemon <maksudyan_mila@mail.ru>
2019-04-16 12:08:41 +0300

    LogIn/SignUp no request to back end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from "./Components/Header";
+import LogIn from "./Components/LogIn";
+import Signup from "./Components/SignUp";
 import NotesPage from "./Components/NotesPage";
+
 
 class App extends Component {
     render() {
         return (
             <div>
+                {/*<LogIn/>*/}
+                <Signup/>
                 <div className="grid_one">
                     <Header/>
                     {/*<NotesPage/>*/}
---
+++ src/Containers/LogInContainer/LogInContainer.js
@@ -0,0 +1,11 @@
+import React, { Component }  from 'react';
+class LogInContainer extends Component {
+
+    render(){
+        return {
+
+        }
+    }
+}
+
+export default LogInContainer;

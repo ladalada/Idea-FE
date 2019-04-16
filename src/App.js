71eb4e5c5b629afd3e966622e62b29b18abacb70
Lada Astashenkova <lada.astashenkova@yandex.ua>
2019-04-16 20:52:33 +0300

    Page404

--- src/App.js
+++ src/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
-  return (
+    return (
       <Router>
           <div>
-              <ul>
-                  <li>
-                      <Link to="/login">Login</Link>
-                  </li>
-                  <li>
-                      <Link to="/signup">SignUp</Link>
-                  </li>
-                  <li>
-                      <Link to="/notes">Notes</Link>
-                  </li>
-                  <li>
-                      <Link to="/notes/:id">NotesID</Link>
-                  </li>
-              </ul>
-
-              <hr />
-
-              <Route path="/login" component={Login} />
-              <Route path="/signup" component={SignUp} />
-              <Route path="/notes" component={Notes} />
-              <Route path="/notes/:id" component={NotesID} />
+              <Route path="/" component={Page404} />
+              <Route path="/login" component={Page404} />
+              <Route path="/signup" component={Page404} />
+              <Route path="/notes" component={Page404} />
+              <Route path="/notes/:id" component={Page404} />
           </div>
       </Router>
   );

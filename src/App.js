f8a8529bac8cf7c1065916461e996a1845ed0b61
Lada Astashenkova <lada.astashenkova@yandex.ua>
2019-04-17 14:12:21 +0300

    Switch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page404 from './Components/Page404'
 
 
 function App() {
     return (
       <Router>
-          <div>
-              <Route path="/" component={Page404} />
+          <Switch>
               <Route path="/login" component={Page404} />
               <Route path="/signup" component={Page404} />
               <Route path="/notes" component={Page404} />
               <Route path="/notes/:id" component={Page404} />
-          </div>
+              <Route path="/" component={Page404} />
+          </Switch>
       </Router>
   );
 }

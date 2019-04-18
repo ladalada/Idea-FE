26de5af8292f961a7bb04950a9d30a5bc601af35
MilonLemon <maksudyan_mila@mail.ru>
2019-04-18 18:53:27 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page404 from './Components/Page404'
 import Start from './Components/Start'
 
-
-function App() {
-    return (
-      <Router>
-          <Switch>
-              <Route path="/login" component={Page404} />
-              <Route path="/signup" component={Page404} />
-              <Route path="/notes" component={Page404} />
-              <Route path="/notes/:id" component={Page404} />
-              <Route path="/" component={Start} />
-          </Switch>
-      </Router>
-  );
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/login" component={Page404}/>
+                    <Route path="/signup" component={Page404}/>
+                    <Route path="/notes" component={Page404}/>
+                    <Route path="/notes/:id" component={Page404}/>
+                    <Route path="/" component={Start}/>
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default App;

6fba923135368a776b375fb23affe3c0396334b5
MilonLemon <maksudyan_mila@mail.ru>
2019-04-18 05:24:12 +0300

    start page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Page404 from './Components/Page404'
+import Start from './Components/Start'
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
               <Route path="/signup" component={Page404} />
               <Route path="/notes" component={Page404} />
               <Route path="/notes/:id" component={Page404} />
-              <Route path="/" component={Page404} />
+              <Route path="/" component={Start} />
           </Switch>
       </Router>
   );
---
+++ src/Components/Start/start.js
@@ -0,0 +1,20 @@
+import './start.css';
+import React, { Component }  from 'react';
+import Idea from '../../Icons/idea.png';
+
+class Start extends Component {
+    render() {
+        return (
+            <div>
+                <img src={Idea} className="idea_icon"/>
+                <div className="idea_title"> Idea </div>
+                <div className="statement">genius ideas <br/> should not <br/> be forgotten</div>
+                <button className="log_in"> Log in </button>
+                <button className="sign_up"> Sign up </button>
+                <button className="question"/>
+            </div>
+        )
+    }
+};
+
+export default Start;

692c649ff606e577af20e9c9f2f072f730bfa814
MilonLemon <maksudyan_mila@mail.ru>
2019-04-18 14:12:50 +0300

    update grid

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Page404 from './Components/Page404'
 import Header from "./Components/Header";
 import NotesPage from "./Components/NotesPage";
 
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <div className="grid_one">
-                    <Header/>
-                    {/*<NotesPage/>*/}
-                </div>
-            </div>
-        )
-    }
-};
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Header/>
+                <Route path="/login" component={Page404} />
+                <Route path="/signup" component={Page404} />
+                <Route path="/notes" component={Page404} />
+                <Route path="/notes/:id" component={Page404} />
+                <Route path="/" component={Page404} />
+            </Switch>
+        </Router>
+    );
+}
 
 export default App;
